d4312ff6f83e8911dcacce298f2dbce715cce523
Mamdouh Issa <m.issa@gcam.gov.sa>
2022-09-06 11:48:20 +0300

    Config Fix

--- src/config/iamshare.php
+++ src/config/iamshare.php
@@ -2,9 +2,13 @@
 
 return [
     'decryption_key' => env('IAMSHARE_DK'),
-    'model' => 'User',
+    'model' => App\Models\User::class,
+    'get_user_url' => env('IAMSHARE_GET_USER_URL'),
     'national_key' => env('IAMSHARE_NI', 'national_id'),
-    'user' => [
-
+    'user_merge' => [
+        'name' => 'arabic_name', // Merge to Created User from the Response
+    ],
+    'default_merge' => [
+        'password' => '--' // Merge default Values.
     ]
 ];
